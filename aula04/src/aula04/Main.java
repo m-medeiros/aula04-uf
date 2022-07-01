846ca91a08d0e94bc4651c744ae8359e8f2f6853
Matheus <43747976+madeiroka@users.noreply.github.com>
2022-07-01 10:47:49 -0300

    Adding code
    Adding everything done in class.

---
+++ aula04/src/aula04/Main.java
@@ -0,0 +1,44 @@
+package aula04;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int fatorial = inputNumber(sc);
+		System.out.printf("Resultado = "+resultFatorial(fatorial, sc));
+	}
+
+	private static boolean checkLog(Scanner sc) {
+		System.out.println("Deseja acompanhar a log dos resultados? (true/false)");
+		sc.nextLine();
+		boolean check = sc.nextBoolean();
+		if (check == true) {
+			return true;
+		}
+		return false;
+		
+	}
+
+	private static int inputNumber(Scanner sc) {		
+		System.out.println("Qual número você quer calcular o fatorial?");
+		int number = sc.nextInt();
+		return number;
+	}
+
+	public static long resultFatorial(int Fatorial, Scanner sc) {
+		long result = 1;
+		if(checkLog(sc) == true) {			
+			for(int i = 1; i<= Fatorial; i++) {
+				result = result * i;
+				System.out.printf(Fatorial+" * "+ i +" = "+result+"\n");
+			}
+		}
+		else {
+			for(int j = 1; j<= Fatorial; j++) {
+			result = result * j;
+		}
+		}
+		return result;
+}
+}
